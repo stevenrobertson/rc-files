7d3b4ee13bc2ee879c38aa5d77027158175b8d62
Steven Robertson <steven@strobe.cc>
2010-06-28 23:29:09 -0400

    whatever

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,36 +1,52 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.Run(spawnPipe, runProcessWithInput)
 import XMonad.Util.EZConfig(additionalKeys)
 import qualified XMonad.StackSet as W
 import XMonad.Layout.Grid
 import XMonad.Layout.ThreeColumns
+import XMonad.Layout.NoBorders
 import System.IO
+import System.Process
 
 modm = mod4Mask
 
-myWorkspaces = map show [1..6]
+normWorkspaces = map show [1..6]
+shiftWorkspaces = map show [7..12]
+myWorkspaces = normWorkspaces ++ shiftWorkspaces
 
 myKeys =
     [
-        ((modm, xK_i), spawn "chromium-bin"),
+        ((modm, xK_i), spawn "chromium"),
         ((modm, xK_t), spawn "xterm")
     ] ++
     [ ((m, k), windows $ f i) |
-        (i, k) <- zip myWorkspaces [xK_F1 .. xK_F6],
-        (f, m) <- [(W.greedyView, 0), (W.shift, modm)]]
+        (i, k) <- zip normWorkspaces [xK_F1 .. xK_F6],
+        (f, m) <- [(W.greedyView, 0), (W.shift, modm)]] ++
+    [ ((m, k), windows $ f i) |
+        (i, k) <- zip shiftWorkspaces [xK_F1 .. xK_F6],
+        (f, m) <- [(W.greedyView, shiftMask), (W.shift, shiftMask .|. modm)]]
+
 
 myLayouts = ThreeColMid 1 (3/100) (1/2) ||| GridRatio (4/3) ||| GridRatio (5/4) ||| Full
 
+launchBar = do
+    -- XMonad might be intercepting the process with an extra wait call?
+    -- out <- readProcess "hostname" [] ""
+    out <- runProcessWithInput "hostname" [] ""
+    let hostname = head $ words out
+    if hostname == "anubis"
+        then spawnPipe "/home/steven/.cabal/bin/xmobar /home/steven/.xmobarrc-anubis"
+        else spawnPipe "xmobar"
 
 main = do
-    xmobar <- spawnPipe "xmobar"
+    xmobar <- launchBar
     xmonad $ defaultConfig {
         workspaces  = myWorkspaces,
         manageHook  = manageDocks <+> manageHook defaultConfig,
-        layoutHook  = avoidStruts  $  myLayouts,
-        logHook = dynamicLogWithPP defaultPP { ppTitle  = shorten 90,
+        layoutHook  = avoidStruts  $  smartBorders $ myLayouts,
+        logHook = dynamicLogWithPP xmobarPP { ppTitle  = shorten 90,
                                                ppLayout = (>> ""),
                                                ppOutput = hPutStrLn xmobar },
         borderWidth = 2,
--- .xstartup.sh
+++ .xstartup.sh
@@ -7,13 +7,15 @@ xmodmap -e "remove Lock = Caps_Lock"
 xmodmap -e "add Mod4 = Caps_Lock"
 
 if [ "`hostname`" = "isis" ]; then
-    $HOME/.scripts/pack_ffox.sh
     smuxi-frontend-gnome &!
 #    xchat &!
 fi
-#compiz --replace --indirect-rendering --loose-binding ccp &!
-#mail-notification --sm-disable &!
-#xmonad &!
+
+if [ "`hostname`" = "anubis" ]; then
+    xmonad &!
+fi
+
+mail-notification --sm-disable &!
 
 if which mitter; then
     mitter &!
@@ -25,10 +27,6 @@ fi
 
 if which pidgin; then
     pidgin &!
-fi
-
-if which tasque; then
-    tasque &!
 fi
 
 
