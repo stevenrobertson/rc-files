0436342e28f8cf0220081e01ba0fb316b17ce78c
Steven Robertson <steven@strobe.cc>
2010-12-14 10:42:58 -0500

    Multi-display code

--- .xstartup.sh
+++ .xstartup.sh
@@ -5,15 +5,18 @@
 
 xrdb .Xdefaults
 
-xmodmap -e "remove Lock = Caps_Lock"
-xmodmap -e "keysym Caps_Lock = Escape"
+#xmodmap -e "remove Lock = Caps_Lock"
+#xmodmap -e "keysym Caps_Lock = Escape"
+xmodmap -e "keycode 66 = Super_L"
 
 if [ "`hostname`" = "isis" ]; then
     pack_ffox.sh
-    /home/steven/.scripts/lql
-    synergys -n localhost
+    DISPLAY=:0.1 /home/steven/.scripts/lql
+    #synergys -n localhost
     DISPLAY=:0.1 xmonad &!
-#    xchat &!
+    DISPLAY=:0.1 xchat &!
+    DISPLAY=:0.1 pidgin &!
+    DISPLAY=:0.2 xmonad &!
 fi
 
 if [ "`hostname`" = "aten" ]; then
@@ -28,9 +31,9 @@ if which pino; then
     pino &!
 fi
 
-if which pidgin; then
-    pidgin &!
-fi
+#if which pidgin; then
+    #pidgin &!
+#fi
 
 if which nvidia-settings; then
     nvidia-settings -l
