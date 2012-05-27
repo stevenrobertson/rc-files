499b1e4c23c6d4b76541a2976a720dedf9b440fa
Steven Robertson <steven@strobe.cc>
2012-05-27 11:49:41 -0700

    xstartup: keep it clean

--- .xstartup.sh
+++ .xstartup.sh
@@ -5,33 +5,4 @@
 
 xrdb .Xdefaults
 [ -f "$HOME/.xmodmap-`hostname`" ] && xmodmap ~/.xmodmap-`hostname`
-
 setxkbmap -option caps:hyper -option compose:rwin
-
-if [ "`hostname`" = "isis" ]; then
-    pack_ffox.sh
-    /home/steven/.scripts/lql &!
-    #xchat &!
-    #pidgin &!
-    synergys -n localhost
-fi
-
-if [ "`hostname`" = "aten" ]; then
-    synergyc -n right isis
-fi
-
-if which mitter; then
-    mitter &!
-fi
-
-if which pino; then
-    pino &!
-fi
-
-#if which pidgin; then
-    #pidgin &!
-#fi
-
-if which nvidia-settings; then
-    nvidia-settings -l
-fi
