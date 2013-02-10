8f9d4f00ecce3dfecc5e55d5ae769ceaef25d619
Steven Robertson <steven@strobe.cc>
2013-02-09 19:13:14 -0800

    Baby's first stock repl fn

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,17 @@
-{:user {:plugins [[lein-droid "0.1.0-SNAPSHOT"]
-                  [lein-tarsier "0.9.3"]
-                  [lein-localrepo "0.4.1"]]
-        :android {:sdk-path "/home/strobe/src/android-sdk-linux_86"}
-        } }
+{:user
+  {:plugins [[lein-droid "0.1.0-SNAPSHOT"]
+             [lein-tarsier "0.9.3"]
+             [lein-localrepo "0.4.1"]]
+   :android {:sdk-path "/home/strobe/src/android-sdk-linux_86"}
+   :jvm-opts ["-Xmx2500M"]
+   :warn-on-reflection true
+   :repl-options {
+    :init (do
+  (require 'clojure.reflect 'clojure.pprint)
+  (defn reflect-find
+    "Reflect an object and find a substring amidst its members."
+    [cls key substr]
+    (clojure.pprint/pprint
+      (filter #(.contains (.toLowerCase (str (key %))) (.toLowerCase substr))
+               (:members (clojure.reflect/reflect cls)))))
+    )}}}
