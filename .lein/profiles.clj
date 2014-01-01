b4a44a2879ed79ad302deb1f55dda178cc708b6d
Steven Robertson <steven@strobe.cc>
2014-01-01 15:01:38 -0800

    Lein update

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,10 +1,9 @@
 {:user
-  {:plugins [[lein-droid "0.1.0-SNAPSHOT"]
-             [lein-tarsier "0.9.3"]
-             [lein-localrepo "0.4.1"]]
+  {:plugins [;[lein-droid "0.1.0-SNAPSHOT"]
+             [lein-localrepo "0.5.1"]]
    :android {:sdk-path "/home/strobe/src/android-sdk-linux_86"}
    :jvm-opts ["-Xmx2500M"]
-   :warn-on-reflection true
+   :global-vars {*warn-on-reflection* true}
    :repl-options {
     :init (do
   (require 'clojure.reflect 'clojure.pprint)
